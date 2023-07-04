cf4270e2945a38cfa87ebefd640b3f5ba6251d8c
EvgeniiKochetov <kochetovevgeny@gmail.com>
2023-07-04 23:09:02 +0300

    Iteration 8 gzip

--- internal/gzip/gzip.go
+++ internal/gzip/gzip.go
@@ -69,28 +69,36 @@ func (c *compressReader) Close() error {
 func MyGzipMiddleware(h http.Handler) http.Handler {
 	gzipFn := func(w http.ResponseWriter, r *http.Request) {
 		ow := w
-		h.ServeHTTP(ow, r)
-		accept := r.Header.Get("Accept-Encoding")
 
-		acceptEncoding := strings.Contains(accept, "gzip")
-		if acceptEncoding {
+		// проверяем, что клиент умеет получать от сервера сжатые данные в формате gzip
+		acceptEncoding := r.Header.Get("Accept-Encoding")
+		supportsGzip := strings.Contains(acceptEncoding, "gzip")
+		if supportsGzip {
+			// оборачиваем оригинальный http.ResponseWriter новым с поддержкой сжатия
 			cw := newCompressWriter(w)
+			// меняем оригинальный http.ResponseWriter на новый
 			ow = cw
-			ow.Header().Set("Accept-Encoding", "gzip")
+			// не забываем отправить клиенту все сжатые данные после завершения middleware
 			defer cw.Close()
 		}
 
+		// проверяем, что клиент отправил серверу сжатые данные в формате gzip
 		contentEncoding := r.Header.Get("Content-Encoding")
 		sendsGzip := strings.Contains(contentEncoding, "gzip")
 		if sendsGzip {
+			// оборачиваем тело запроса в io.Reader с поддержкой декомпрессии
 			cr, err := newCompressReader(r.Body)
 			if err != nil {
 				w.WriteHeader(http.StatusInternalServerError)
 				return
 			}
+			// меняем тело запроса на новое
 			r.Body = cr
 			defer cr.Close()
 		}
+
+		// передаём управление хендлеру
+		h.ServeHTTP(ow, r)
 
 	}
 
