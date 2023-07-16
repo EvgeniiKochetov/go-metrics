0ae37aa456e29fbbeb8c480eafbb0bd9ef4633c4
EvgeniiKochetov <kochetovevgeny@gmail.com>
2023-07-16 16:35:13 +0300

    Iteration 10

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -14,6 +14,7 @@ import (
 	"github.com/EvgeniiKochetov/go-metrics-tpl/internal/handler"
 	"github.com/EvgeniiKochetov/go-metrics-tpl/internal/logger"
 	"github.com/go-chi/chi/v5"
+	_ "github.com/jackc/pgx"
 	"go.uber.org/zap"
 )
 
