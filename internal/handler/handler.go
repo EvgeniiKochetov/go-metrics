f2be09bfe6073790d12905502dbac3945be37427
EvgeniiKochetov <kochetovevgeny@gmail.com>
2023-07-16 20:00:13 +0300

    Iteration 10

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -247,11 +247,12 @@ func ValueUseJSON(w http.ResponseWriter, r *http.Request) {
 func Ping(w http.ResponseWriter, r *http.Request) {
 	db := config.GetInstance().GetDatabaseConnection()
 
-	fmt.Println("Ping: ", db)
+	fmt.Println("Ping: ")
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 	if err := db.PingContext(ctx); err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Println("Ping end: ", w.Header())
 		return
 	}
 	w.WriteHeader(http.StatusOK)
