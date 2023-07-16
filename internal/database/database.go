83d4f2a9c836b837e14640938ddb763d0b5408c1
EvgeniiKochetov <kochetovevgeny@gmail.com>
2023-07-16 21:54:35 +0300

    Iteration 11

--- internal/database/database.go
+++ internal/database/database.go
@@ -5,13 +5,17 @@ import (
 	"fmt"
 )
 
-func tableExists(db *sql.DB) error {
-
-	return nil
+func tableExists(db *sql.DB) (bool, error) {
+	r := "SELECT EXISTS (SELECT * FROM pg_tables WHERE tablename = 'Metrics')"
+	_, err := db.Exec(r)
+	if err != nil {
+		return false, err
+	}
+	return true, nil
 }
 
 func CreateTable(db *sql.DB) error {
-	createTable := fmt.Sprintf("CREATE TABLE Metrics (type varchar(10), name varchar(50), value DOUBLE PRECISION, counter integer)")
+	createTable := "DELETE TABLE Metics; CREATE TABLE Metrics (type varchar(10), name varchar(50), value DOUBLE PRECISION, counter integer)"
 
 	_, err := db.Exec(createTable)
 	if err != nil {
@@ -19,4 +23,22 @@ func CreateTable(db *sql.DB) error {
 		return err
 	}
 	return nil
+}
+
+func AddGaugeMetric(db *sql.DB, nameOfMetric string, value float64) error {
+	return nil
+}
+
+func AddCounterMetric(db *sql.DB, nameOfMetric string, value float64) error {
+	return nil
+}
+
+func GetGaugeMetric(db *sql.DB, nameOfMetric string) (*float64, error) {
+	pointer := float64(0)
+	return &pointer, nil
+}
+
+func GetCounterMetric(db *sql.DB, nameOfMetric string) (*int64, error) {
+	pointer := int64(0)
+	return &pointer, nil
 }
